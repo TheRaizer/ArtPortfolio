faa1d15900e7fede7e6c30f26efa48e2ff38aed1
Aidan Fu <aidan.fu000@gmail.com>
2022-09-29 23:49:17 -0400

    setup pepsico

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,9 +1,52 @@
-import { ReactElement } from 'react';
+import { motion, Variants } from 'framer-motion';
+import { ReactElement, useMemo, useState } from 'react';
+import styled from 'styled-components';
+import { DimensionProps } from '../../../types/Dimension.type';
+import { Col } from '../common/Col';
 import { FormInput } from './FormInput';
 
+const Styled = {
+  ButtonContainer: styled.div`
+    width: 150px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  SubmitButton: styled(motion.button)<DimensionProps & { sent: boolean }>`
+    width: ${({ width }) => width};
+    height: ${({ height }) => height};
+    border-radius: 3px;
+    background-color: ${({ sent }) => (sent ? 'gray' : 'black')};
+
+    transition: width 200ms linear;
+  `,
+};
+
+const submitButtonVariants: Variants = {
+  initial: {
+    scale: 1,
+    transition: {
+      duration: 0.2,
+      type: 'spring',
+    },
+  },
+  sent: {
+    scale: 0.9,
+    transition: {
+      duration: 0.2,
+      type: 'spring',
+    },
+  },
+};
+
 export const ContactForm = (): ReactElement => {
+  const [sent, setSent] = useState(false);
+  const animation = useMemo(() => (sent ? 'initial' : 'sent'), [sent]);
+  const buttonText = useMemo(() => (sent ? 'Sent!' : 'Submit'), [sent]);
+
   return (
-    <>
+    <Col gap="20px">
       <FormInput
         label="Name (required)"
         width="300px"
@@ -20,12 +63,26 @@ export const ContactForm = (): ReactElement => {
       />
       <FormInput
         label="Message"
-        width="400px"
+        width="600px"
         height="300px"
         fontSize="15px"
         labelSize="14px"
         isTextArea={true}
       />
-    </>
+      <Styled.ButtonContainer>
+        <Styled.SubmitButton
+          initial="initial"
+          whileTap={animation}
+          onClick={() => setSent(true)}
+          variants={submitButtonVariants}
+          width="150px"
+          height="50px"
+          sent={sent}
+          disabled={sent}
+        >
+          {buttonText}
+        </Styled.SubmitButton>
+      </Styled.ButtonContainer>
+    </Col>
   );
 };
