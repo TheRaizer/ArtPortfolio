6f9947f861b1c269b0d3d14f122c87f919421ca4
Aidan Fu <aidan.fu000@gmail.com>
2022-10-03 22:15:27 -0300

    push file name changes

---
+++ src/__test__/components/Nav/Navbar.test.tsx
@@ -0,0 +1,44 @@
+import { screen, waitFor } from '@testing-library/react';
+import React from 'react';
+import { Navbar } from '../../../components/Nav/Navbar';
+import { Pages } from '../../../constants/pages';
+import { render } from '../../utils/custom-renderer';
+
+describe('Navbar', () => {
+  it('should contain the correct sections text', () => {
+    render(<Navbar page={Pages.HOME} />);
+
+    expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /gallery/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /contact/i })).toBeInTheDocument();
+  });
+
+  describe('Visibility', () => {
+    it('should not be visible initially', () => {
+      render(<Navbar page={Pages.HOME} />);
+
+      expect(screen.getByRole('link', { name: /home/i })).not.toBeVisible();
+      expect(screen.getByRole('link', { name: /about/i })).not.toBeVisible();
+      expect(screen.getByRole('link', { name: /gallery/i })).not.toBeVisible();
+      expect(screen.getByRole('link', { name: /contact/i })).not.toBeVisible();
+    });
+
+    it('should become visible', async () => {
+      render(<Navbar page={Pages.HOME} />);
+
+      await waitFor(() =>
+        expect(screen.getByRole('link', { name: /home/i })).toBeVisible()
+      );
+      await waitFor(() =>
+        expect(screen.getByRole('link', { name: /about/i })).toBeVisible()
+      );
+      await waitFor(() =>
+        expect(screen.getByRole('link', { name: /gallery/i })).toBeVisible()
+      );
+      await waitFor(() =>
+        expect(screen.getByRole('link', { name: /contact/i })).toBeVisible()
+      );
+    });
+  });
+});
---
+++ src/components/Nav/Navbar.tsx
@@ -0,0 +1,67 @@
+import { ReactElement, useEffect, useMemo } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { ViewportStates } from '../../../types/recoil/atoms/appConfig.type';
+import { Pages } from '../../constants/pages';
+import { appConfigState } from '../../recoil/atoms/appConfig';
+import { mobileNavIsOpenSelector } from '../../recoil/selectors/mobileNavSelector';
+import { DesktopNavbar } from './Desktop/DesktopNavbar';
+import { DesktopNavItem } from './Desktop/DesktopNavItem';
+import { MobileNav } from './Mobile/MobileNav';
+import { MobileNavItem } from './Mobile/MobileNavItem';
+
+export const Navbar = ({
+  page,
+  className,
+}: {
+  page: Pages;
+  className?: string;
+}): ReactElement => {
+  const { viewportState } = useRecoilValue(appConfigState);
+  const setMobileNavIsOpen = useSetRecoilState(mobileNavIsOpenSelector);
+
+  const NavComponent = useMemo(
+    () =>
+      viewportState === ViewportStates.MOBILE ? (
+        <MobileNav className={className}>
+          <MobileNavItem
+            href={'/'}
+            initialOpacity={page === Pages.HOME ? 1 : undefined}
+          >
+            Home
+          </MobileNavItem>
+          <MobileNavItem href={'#about'}>About</MobileNavItem>
+          <MobileNavItem href={'#gallery'}>Gallery</MobileNavItem>
+          <MobileNavItem
+            href={Pages.CONTACT}
+            initialOpacity={page === Pages.CONTACT ? 1 : undefined}
+          >
+            Contact
+          </MobileNavItem>
+        </MobileNav>
+      ) : (
+        <DesktopNavbar delay={page !== Pages.CONTACT} className={className}>
+          <DesktopNavItem
+            href={'/'}
+            initialOpacity={page === Pages.HOME ? 1 : undefined}
+          >
+            Home
+          </DesktopNavItem>
+          <DesktopNavItem href={'#about'}>About</DesktopNavItem>
+          <DesktopNavItem href={'#gallery'}>Gallery</DesktopNavItem>
+          <DesktopNavItem
+            href={Pages.CONTACT}
+            initialOpacity={page === Pages.CONTACT ? 1 : undefined}
+          >
+            Contact
+          </DesktopNavItem>
+        </DesktopNavbar>
+      ),
+    [className, page, viewportState]
+  );
+
+  useEffect(() => {
+    if (viewportState !== ViewportStates.MOBILE) setMobileNavIsOpen(false);
+  }, [setMobileNavIsOpen, viewportState]);
+
+  return NavComponent;
+};
